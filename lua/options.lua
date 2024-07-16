3c825a60d9ef8dd6250c71ae35c8c5565e3acc1a
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-07-17 02:23:01 +0500

    refactor: options file

--- lua/options.lua
+++ lua/options.lua
@@ -1,18 +1,51 @@
+-- Set space as leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.o.clipboard = 'unnamedplus'     -- Use system clipboard
-vim.o.relativenumber = true         -- Relative line numbers in gutter
-vim.o.tabstop = 4                   -- Number of spaces per tab
-vim.o.shiftwidth = 4                -- Number of spaces to use for indents with >>, << and ==
-vim.o.updatetime = 300
-vim.o.termguicolors = true          -- 24 bit colours
-vim.o.mouse = 'a'                   -- Enable mouse support for all modes
-vim.opt.exrc = true                 -- Use local .nvimrc if it exists
-vim.opt.secure = true               -- Disable unsafe commands in local .nvimrc
-vim.o.cursorline = true             -- Highlight current line
-vim.o.fillchars = 'eob: '           -- Hide ~ at end of buffers
-vim.o.laststatus = 3                -- Ensure global statusline is always visible
-vim.o.signcolumn = 'yes'			-- Always show sign column
+-- Use system clipboard
+vim.o.clipboard = 'unnamedplus'
 
+-- Relative line numbers in gutter
+vim.o.relativenumber = true
+
+-- Number of spaces per tab
+vim.o.tabstop = 4
+
+-- Number of spaces to use for indents with >>, << and ==
+vim.o.shiftwidth = 4
+
+-- 24 bit colours
+vim.o.termguicolors = true
+
+-- Enable mouse support for all modes
+vim.o.mouse = 'a'
+
+-- Use local .nvimrc if it exists
+vim.opt.exrc = true
+
+-- Disable unsafe commands in local .nvimrc
+vim.opt.secure = true
+
+-- Highlight current line
+vim.o.cursorline = true
+
+-- Hide ~ at end of buffers
+vim.o.fillchars = 'eob: '
+
+-- Ensure global statusline is always visible
+vim.o.laststatus = 3
+
+-- Always show sign column
+vim.o.signcolumn = 'yes'
+
+-- Undo files
+vim.opt.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Line wrap
 vim.o.wrap = false
+
+vim.o.updatetime = 300
