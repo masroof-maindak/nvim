635e6a4aa7655ea5efbf04542a84cc66bdb5ef70
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-07-17 02:20:36 +0500

    feat: attempt to set up LSP

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -7,3 +7,44 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.linebreak = true
 	end,
 })
+
+-- LSP
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('user_lsp_attach', { clear = true }),
+	callback = function(event)
+		print('LSP attached to buffer ' .. event.buf)
+		local opts = { buffer = event.buf }
+		vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+		vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+		vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
+		vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+		vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+		vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+		vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+		vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+		vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+		vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+	end,
+})
+
+-- C/C++ LSP
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "c", "cpp" },
+	callback = function()
+		local lspconfig = require('lspconfig')
+		lspconfig.clangd.setup {
+			on_attach = _G.lsp_on_attach,
+		}
+	end,
+})
+
+-- Go LSP
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "go" },
+	callback = function()
+		local lspconfig = require('lspconfig')
+		lspconfig.gopls.setup {
+			on_attach = _G.lsp_on_attach,
+		}
+	end,
+})
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,40 @@
+return {
+    { -- LSP
+        {
+            "williamboman/mason.nvim",
+            opts = {
+                ui = {
+                    icons = {
+                        package_installed = "🗹",
+                        package_pending = "➜",
+                        package_uninstalled = "✗"
+                    },
+                },
+            },
+        },
+        {
+            "williamboman/mason-lspconfig.nvim",
+            opts = {
+                ensure_installed = {
+                    "clangd",
+                    "gopls"
+                },
+            },
+        },
+        {
+            "neovim/nvim-lspconfig",
+            config = function()
+                local lspconfig = require('lspconfig')
+
+                -- Define a function for common on_attach settings
+                local on_attach = function(client, bufnr)
+                    vim.api.nvim_exec_autocmds('LspAttach', { buffer = bufnr })
+                end
+
+                -- Make the on_attach function available globally
+                _G.lsp_on_attach = on_attach
+            end,
+        },
+        event = "VeryLazy",
+    },
+}
