6449a53d8c7bbfc35b1922d4164d5f56fca9bda8
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-07-17 22:28:01 +0500

    feat: add formatting via conform and mason-conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,53 @@
+local formatters = {
+	"stylua",
+	"pretty_php",
+	"prettierd",
+	"clang-format",
+}
+
+return {
+	{ -- Configure formatters
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true, lsp_format = "fallback" })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		opts = {
+			formatters_by_ft = {
+				c = { { "clang-format" } },
+				["c++"] = { { "clang-format" } },
+				javascript = { { "biome" } },
+				lua = { { "stylua" } },
+				php = { { "pretty-php" } },
+				bash = { { "shfmt" } },
+			},
+
+			-- format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+			format_on_save = nil,
+
+			formatters = {
+				shfmt = {
+					prepend_args = { "-i", "2" },
+				},
+			},
+		},
+		after = "mason.nvim",
+		init = function()
+			-- If you want the formatexpr, here is the place to set it
+			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+		end,
+	},
+	{ -- Automatically install formatters
+		"zapling/mason-conform.nvim",
+		opts = {},
+		after = "conform.nvim",
+	},
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,48 +1,48 @@
 local servers = {
-    "clangd",
-    "gopls",
-    "intelephense",
-    "tsserver",
-    "bashls",
-    "dockerls",
+	"clangd",
+	"gopls",
+	"intelephense",
+	"tsserver",
+	"bashls",
+	"dockerls",
 }
 
 return {
-    { -- LSP
-        {
-            "williamboman/mason.nvim",
-            opts = {
-                ui = {
-                    icons = {
-                        package_installed = "🗹",
-                        package_pending = "➜",
-                        package_uninstalled = "✗"
-                    },
-                },
-            },
-        },
-        {
-            "williamboman/mason-lspconfig.nvim",
-            opts = {
-                ensure_installed = servers,
-            },
-        },
-        {
-            "neovim/nvim-lspconfig",
-            config = function()
-                local lspconfig = require('lspconfig')
+	{ -- LSP
+		{
+			"williamboman/mason.nvim",
+			opts = {
+				ui = {
+					icons = {
+						package_installed = "🗹",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			},
+		},
+		{
+			"williamboman/mason-lspconfig.nvim",
+			opts = {
+				ensure_installed = servers,
+			},
+		},
+		{
+			"neovim/nvim-lspconfig",
+			config = function()
+				local lspconfig = require("lspconfig")
 
-                local on_attach = function(client, bufnr)
-                    vim.api.nvim_exec_autocmds('LspAttach', { buffer = bufnr })
-                end
+				local on_attach = function(client, bufnr)
+					vim.api.nvim_exec_autocmds("LspAttach", { buffer = bufnr })
+				end
 
-                for _, lsp in ipairs(servers) do
-                    lspconfig[lsp].setup {
-                        on_attach = on_attach,
-                    }
-                end
-            end,
-        },
-        event = "VeryLazy",
-    },
+				for _, lsp in ipairs(servers) do
+					lspconfig[lsp].setup({
+						on_attach = on_attach,
+					})
+				end
+			end,
+		},
+		event = "VeryLazy",
+	},
 }
