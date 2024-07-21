45d8ee8e931e094d3d024e5aeb4ab7f33c4b613e
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-07-21 18:55:19 +0500

    feat: <leader> + q to quit keybind

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -6,11 +6,14 @@ key("i", "jk", "<Esc>", { noremap = true, silent = true })
 -- Save with 'Ctrl-s'
 key("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
 
--- Quit all buffers with 'Ctrl-q'
+-- Quit/Quit All
 key("n", "<C-q>", ":qa<CR>", { noremap = true, silent = true })
+key("n", "<leader>q", ":q<CR>", { noremap = true, silent = true })
 
 -- File opener with '-'
-key("n", "-", function() MiniFiles.open(vim.api.nvim_buf_get_name(0)) end)
+key("n", "-", function()
+	MiniFiles.open(vim.api.nvim_buf_get_name(0))
+end)
 
 -- Delete current buffer
 key("n", "<leader>bd", "<CMD>bd<CR>", { noremap = true })
@@ -22,39 +25,39 @@ key("n", "<leader>gs", "<CMD>Pick grep live<CR>", { noremap = true, desc = "Live
 
 -- Move to the beginning/end of the line
 key("n", "L", "$", { noremap = true, silent = true })
-key("n", "H", "^", { noremap = true, silent = true }) 
+key("n", "H", "^", { noremap = true, silent = true })
 key("v", "L", "g_", { noremap = true, silent = true })
 key("v", "H", "^", { noremap = true, silent = true })
 
 -- 'Tab' cycles through completion items
-key('i', '<Tab>',   [[pumvisible() ? "\<C-n>" : "\<Tab>"]],   { expr = true })
-key('i', '<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { expr = true })
+key("i", "<Tab>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]], { expr = true })
+key("i", "<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { expr = true })
 
 -- Approve completion item
-key('i', '<leader>a', [[pumvisible() ? "\<C-y>" : "\<CR>"]], { expr = true })
+key("i", "<leader>a", [[pumvisible() ? "\<C-y>" : "\<CR>"]], { expr = true })
 
 -- Window navigation with 'Ctrl-hjkl'
-key('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
-key('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
-key('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
-key('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+key("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+key("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+key("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+key("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 
 -- Window resize with 'Ctrl-arrow'
-key('n', '<C-left>',  '<C-w><', { noremap = true, silent = true })
-key('n', '<C-down>',  '<C-w>-', { noremap = true, silent = true })
-key('n', '<C-up>',    '<C-w>+', { noremap = true, silent = true })
-key('n', '<C-right>', '<C-w>>', { noremap = true, silent = true })
+key("n", "<C-left>", "<C-w><", { noremap = true, silent = true })
+key("n", "<C-down>", "<C-w>-", { noremap = true, silent = true })
+key("n", "<C-up>", "<C-w>+", { noremap = true, silent = true })
+key("n", "<C-right>", "<C-w>>", { noremap = true, silent = true })
 
 -- Cycle to the next buffer with 'Tab'
-key('n', '<Tab>', ':bn<CR>', { noremap = true, silent = true })
-key('n', '<S-Tab>', ':bp<CR>', { noremap = true, silent = true })
+key("n", "<Tab>", ":bn<CR>", { noremap = true, silent = true })
+key("n", "<S-Tab>", ":bp<CR>", { noremap = true, silent = true })
 
 -- Clear search highlights with 'Esc'
-key('n', '<Esc>', ':noh<CR><Esc>', { noremap = true, silent = true })
+key("n", "<Esc>", ":noh<CR><Esc>", { noremap = true, silent = true })
 
 -- Remain in visual selection after shifting text
-key('v', '<', '<gv', { noremap = true, silent = true })
-key('v', '>', '>gv', { noremap = true, silent = true })
+key("v", "<", "<gv", { noremap = true, silent = true })
+key("v", ">", ">gv", { noremap = true, silent = true })
 
 -- Exit terminal with double Esc
-key('t', '<Esc><Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+key("t", "<Esc><Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
