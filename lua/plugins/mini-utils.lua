412729402628549779b83d934320b47b330e7f0a
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-07-21 18:12:27 +0500

    feat: git plugins

--- lua/plugins/mini-utils.lua
+++ lua/plugins/mini-utils.lua
@@ -1,19 +1,23 @@
 return {
-    {
-        'echasnovski/mini.files',
-        version = false,
-        opts = { content = { prefix = function() end }, },     -- remove icons
-        event = "VeryLazy"
-    },
-    { 'echasnovski/mini.pick', version = false, opts = {}, event = "VeryLazy", },
-    {
-        'echasnovski/mini.completion',
-        version = false,
-        opts = {
-            lsp_completion = {
-                source_func = 'omnifunc',
-                auto_setup = true,
-            }
-        }
-    }
+	{
+		"echasnovski/mini.files",
+		version = false,
+		opts = { content = { prefix = function() end } }, -- remove icons
+		event = "VeryLazy",
+	},
+	{ "echasnovski/mini.pick", version = false, opts = {}, event = "VeryLazy" },
+	{ -- LSP Completion
+		"echasnovski/mini.completion",
+		version = false,
+		opts = {
+			lsp_completion = {
+				source_func = "omnifunc",
+				auto_setup = true,
+			},
+		},
+	},
+	{ -- GIT
+		{ "echasnovski/mini-git", version = false, main = "mini.git", opts = {}, event = "VeryLazy" },
+		{ "echasnovski/mini.diff", version = false, opts = {}, event = "VeryLazy" },
+	},
 }
