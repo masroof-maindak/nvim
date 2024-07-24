04858b8b5b2e76914f4a449d97338dc0553aae7a
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-07-25 02:57:34 +0500

    patch: git diff sign blocks via mini.diff

--- lua/plugins/mini-utils.lua
+++ lua/plugins/mini-utils.lua
@@ -18,6 +18,15 @@ return {
 	},
 	{ -- GIT
 		{ "echasnovski/mini-git", version = false, main = "mini.git", opts = {}, event = "VeryLazy" },
-		{ "echasnovski/mini.diff", version = false, opts = {}, event = "VeryLazy" },
+		{
+			"echasnovski/mini.diff",
+			version = false,
+			opts = {
+				view = {
+					style = 'sign',
+				},
+			},
+			event = "VeryLazy"
+		},
 	},
 }
