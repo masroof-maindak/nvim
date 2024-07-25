ce4a7afe9e7543fb30a7e63e1e5ee974686fa43e
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-07-26 00:41:46 +0500

    chore: diagnostic highlights adjusted to match background

--- colors/swamp.lua
+++ colors/swamp.lua
@@ -43,23 +43,29 @@ if vim.o.background == "dark" then
 end
 
 if palette then
-	require('mini.base16').setup({ palette = palette })
-	vim.g.colors_name = 'base16-swamp'
-	vim.cmd [[ highlight WinSeparator guibg=None ]]
+	require("mini.base16").setup({ palette = palette })
+	vim.g.colors_name = "base16-swamp"
+	vim.cmd([[ highlight WinSeparator guibg=None ]])
 end
 
 local function hi(group, opts)
 	vim.api.nvim_set_hl(0, group, opts)
 end
 
-hi('LineNr', { fg = palette.base05, bg = palette.base00, attr = nil, sp = nil })
-hi('LineNrAbove', { fg = palette.base02, bg = palette.base00, attr = nil, sp = nil })
-hi('LineNrBelow', { fg = palette.base02, bg = palette.base00, attr = nil, sp = nil })
+hi("LineNr", { fg = palette.base05, bg = palette.base00, attr = nil, sp = nil })
+hi("LineNrAbove", { fg = palette.base02, bg = palette.base00, attr = nil, sp = nil })
+hi("LineNrBelow", { fg = palette.base02, bg = palette.base00, attr = nil, sp = nil })
 
-hi('MiniDiffSignAdd', { fg = palette.base0B, bg = palette.base00, attr = nil, sp = nil })
-hi('MiniDiffSignChange', { fg = palette.base0E, bg = palette.base00, attr = nil, sp = nil })
-hi('MiniDiffSignDelete', { fg = palette.base08, bg = palette.base00, attr = nil, sp = nil })
+hi("MiniDiffSignAdd", { fg = palette.base0B, bg = palette.base00, attr = nil, sp = nil })
+hi("MiniDiffSignChange", { fg = palette.base0E, bg = palette.base00, attr = nil, sp = nil })
+hi("MiniDiffSignDelete", { fg = palette.base08, bg = palette.base00, attr = nil, sp = nil })
 
-hi('SignColumn', { fg = palette.base03, bg = palette.base00, attr = nil, sp = nil })
+hi("SignColumn", { fg = palette.base03, bg = palette.base00, attr = nil, sp = nil })
 
-hi('CursorLineNr', { fg = palette.base05, bg = palette.base01, attr = nil, sp = nil })
+hi("CursorLineNr", { fg = palette.base05, bg = palette.base01, attr = nil, sp = nil })
+
+hi("DiagnosticFloatingError", { fg = palette.base08, bg = palette.base00, attr = nil, sp = nil })
+hi("DiagnosticFloatingHint", { fg = palette.base0D, bg = palette.base00, attr = nil, sp = nil })
+hi("DiagnosticFloatingInfo", { fg = palette.base0C, bg = palette.base00, attr = nil, sp = nil })
+hi("DiagnosticFloatingOk", { fg = palette.base0B, bg = palette.base00, attr = nil, sp = nil })
+hi("DiagnosticFloatingWarn", { fg = palette.base0E, bg = palette.base00, attr = nil, sp = nil })
