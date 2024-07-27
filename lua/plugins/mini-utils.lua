553ac9e9a7db41f16fc655c81a81306c1f6853aa
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-07-27 17:42:23 +0500

    patch: centered mini.pick

--- lua/plugins/mini-utils.lua
+++ lua/plugins/mini-utils.lua
@@ -2,10 +2,39 @@ return {
 	{
 		"echasnovski/mini.files",
 		version = false,
-		opts = { content = { prefix = function() end } }, -- remove icons
+		opts = {
+			content = { prefix = function() end },
+		}, -- remove icons
 		event = "VeryLazy",
 	},
-	{ "echasnovski/mini.pick", version = false, opts = {}, event = "VeryLazy" },
+	{
+		"echasnovski/mini.pick",
+		version = false,
+		config = function()
+			local pick = require("mini.pick")
+			local win_config = function()
+				local height = math.floor(0.618 * vim.o.lines)
+				local width = math.floor(0.618 * vim.o.columns)
+				return {
+					anchor = "NW",
+					height = height,
+					width = width,
+					row = math.floor(0.5 * (vim.o.lines - height)),
+					col = math.floor(0.5 * (vim.o.columns - width)),
+					border = "none",
+				}
+			end
+			pick.setup({
+				source = {
+					show = pick.default_show, -- remove icons
+				},
+				window = {
+					config = win_config, -- center pick
+				},
+			})
+		end,
+		event = "VeryLazy",
+	},
 	{ -- LSP Completion
 		"echasnovski/mini.completion",
 		version = false,
