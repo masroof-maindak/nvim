b95accfc0272f3ae600b0b7f92d115076d9e18ba
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-09-09 22:36:45 +0500

    chore: cleaning & proper Lazy events

--- lua/plugins/mini-git.lua
+++ lua/plugins/mini-git.lua
@@ -3,18 +3,19 @@ return {
 		{
 			"echasnovski/mini-git",
 			version = false,
+			event = "VeryLazy",
 			main = "mini.git",
 			opts = {},
 		},
 		{
 			"echasnovski/mini.diff",
 			version = false,
+			event = "VeryLazy",
 			opts = {
 				view = {
 					style = "sign",
 				},
 			},
 		},
-		event = "VeryLazy",
 	},
 }
--- lua/plugins/mini-ui.lua
+++ lua/plugins/mini-ui.lua
@@ -54,23 +54,19 @@ return {
 					local pathname = vim.bo.buftype == "terminal" and "%t"
 						or "%#MiniStatuslineFilename#" .. vim.fn.expand("%:t") .. (vim.bo.modified and " [+]" or "")
 
-					local devinfo = ""
-					if git ~= "" and diff ~= " -" then
-						devinfo = git .. " │" .. diff
-					elseif git ~= "" then
-						devinfo = git .. " "
-					elseif diff ~= " -" then
-						devinfo = diff
+					local devinfo
+					if git == "" then
+						devinfo = diff == " -" and "" or diff
+					else
+						devinfo = git .. (diff == " -" and " " or (" │" .. diff))
 					end
 
 					return MiniStatusline.combine_groups({
 						{ hl = mode_hl, strings = { mode:upper() } },
 						{ hl = "MiniStatuslineDevinfo", strings = { diagnostics } },
-
 						"%<", -- Mark general truncate point
 						{ hl = "MiniStatuslineFilename", strings = { pathname } },
 						"%=", -- End left alignment
-
 						{ hl = "MiniStatuslineDevinfo", strings = { devinfo } },
 						{ hl = mode_hl, strings = { "%-2l,%-2v" } },
 					})
