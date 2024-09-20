e56cf68294ba9d28f987bba34a257a4ef90bfd83
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-09-21 01:00:41 +0500

    chore: kill nerd icons in which-key + mini.pick closer to default

--- lua/plugins/mini-utils.lua
+++ lua/plugins/mini-utils.lua
@@ -12,25 +12,10 @@ return {
 		version = false,
 		config = function()
 			local pick = require("mini.pick")
-			local win_config = function()
-				local height = math.floor(0.618 * vim.o.lines)
-				local width = math.floor(0.618 * vim.o.columns)
-				return {
-					anchor = "NW",
-					height = height,
-					width = width,
-					row = math.floor(0.5 * (vim.o.lines - height)),
-					col = math.floor(0.5 * (vim.o.columns - width)),
-					border = "none",
-				}
-			end
 			pick.setup({
 				source = {
-					show = pick.default_show, -- remove icons
-				},
-				window = {
-					config = win_config, -- center pick
-				},
+					show = pick.default_show,
+				}, -- remove icons
 			})
 		end,
 		event = "VeryLazy",
