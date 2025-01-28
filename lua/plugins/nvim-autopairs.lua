865f981c4bde0641b5adf43f1f4bcafcfae5fa67
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-01-28 13:34:47 +0500

    patch: replace mini.pairs w/ nvim-autopairs

---
+++ lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,17 @@
+return {
+	'windwp/nvim-autopairs',
+	event = "InsertEnter",
+	config = function()
+		local np = require("nvim-autopairs")
+		local Rule = require("nvim-autopairs.rule")
+		local cond = require("nvim-autopairs.conds")
+
+		np.setup({})
+
+		np.add_rules({
+			Rule("*", "*", { "markdown" }):with_pair(cond.not_before_regex("\n")),
+			Rule("_", "_", { "markdown" }):with_pair(cond.before_regex("%s")),
+		})
+	end,
+}
+
