f6a4013684bbed113decacf62f6e888d3232046e
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-10-13 05:21:16 +0500

    patch: remove mini.comment & mini.jump

--- lua/plugins/mini-editing.lua
+++ lua/plugins/mini-editing.lua
@@ -1,15 +1,8 @@
 return {
-	{ "nvim-mini/mini.comment", version = false, opts = {}, event = "VeryLazy" },
 	{
 		"nvim-mini/mini.surround",
 		version = false,
 		opts = { highlight_duration = 1000 },
-		event = "VeryLazy",
-	},
-	{
-		"nvim-mini/mini.jump",
-		version = false,
-		opts = { delay = { idle_stop = 5000 } },
 		event = "VeryLazy",
 	},
 }
